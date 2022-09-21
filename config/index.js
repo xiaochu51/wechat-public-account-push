20e34922cef9150e2711f323fbb1901d58099713
xiaochu51 <114054857+xiaochu51@users.noreply.github.com>
2022-09-21 15:20:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wx9c8e745f7fc2046c",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "f4461eba894f4101cff4bb56ccf36639",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -62,9 +62,9 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "wx9c8e745f7fc2046c",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "9aHH7wtBE8ml93GQdRHX3vm-dDT194ZHgqrZJX-w7vc",
       // 所在省份
       province: "广东",
       // 所在城市
@@ -87,11 +87,9 @@ export const config = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2022-09-08"},
-        // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
-        // 退伍日
-        {"keyword": "ex_day", date: "2022-09-10"},
+        {"keyword": "love_day", date: "2022-05-01"},
+        // 宝贝的生日
+        {"keyword": "marry_day", date: "2022-11-12"}
       ]
     },
     {
